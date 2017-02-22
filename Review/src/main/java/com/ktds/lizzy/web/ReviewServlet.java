efe60ed2319b9e5fa963a41dcee88dd18ae6cf67
Lizzy <lizzysoo0000@gmail.com>
2017-02-22 09:43:21 +0900

    [170221] Jsp & Servlet 활용 (STS 사용)

---
+++ Review/src/main/java/com/ktds/lizzy/web/ReviewServlet.java
@@ -0,0 +1,45 @@
+package com.ktds.lizzy.web;
+
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class ReviewServlet
+ */
+public class ReviewServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public ReviewServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		request.setAttribute("name", "Lizzy");
+		request.setAttribute("age", "24");
+		request.setAttribute("alias", "대현동 불주먹");
+		
+		RequestDispatcher dispatcher = request.getRequestDispatcher("/review.jsp");
+		dispatcher.forward(request, response);
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
---
+++ Review/src/main/webapp/review.jsp
@@ -0,0 +1,19 @@
+<%@ page language="java" contentType="text/html; charset=EUC-KR"
+    pageEncoding="EUC-KR"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=EUC-KR">
+<title>My Page</title>
+</head>
+<body>
+
+	<h1>My name</h1>
+	<h2>${name}</h2>
+	<h1>Age</h1>
+	<h2>${age}</h2>
+	<h1>Alias</h1>
+	<h2>${alias}</h2>
+
+</body>
+</html>
